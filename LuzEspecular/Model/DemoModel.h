bb716776094503540cb20b6b88f543e0b03dd8f4
DharmaSandoval <dharmasanchez76@gmail.com>
2023-10-17 22:09:35 -0600

    Practica Luz Especular
    Luz Especular Rotatoria

---
+++ LuzEspecular/Model/DemoModel.h
@@ -0,0 +1,74 @@
+// DemoModel.h : Defines the CDemoModel class.
+//
+//
+//
+
+#ifndef _DEMOMODEL_H_
+#define _DEMOMODEL_H_
+
+#include "Dx11Base.h"
+#include "ObjParser.h"
+
+
+class CDemoModel : public CDx11Base
+{
+// Constructors
+public:
+    CDemoModel(float specular);
+    virtual ~CDemoModel();
+
+// Overrides
+public:
+    virtual bool LoadContent();
+    virtual void UnloadContent();
+
+    virtual void Update();
+    virtual void Render();
+
+// Implementation
+protected:
+    void CalcSphericalCamera();
+    XMFLOAT3 UpdateColor(XMFLOAT3);
+    XMFLOAT3 ChangeColor(XMFLOAT3 arcoiris);
+
+// Members
+protected:
+    ID3D11VertexShader* m_pVS;
+    ID3D11PixelShader* m_pPS;
+    ID3D11InputLayout* m_pInputLayout;
+    ID3D11Buffer* m_pVertexBuffer;
+    ID3D11ShaderResourceView* m_pColorMap;
+    ID3D11SamplerState* m_pColorMapSampler;
+    ID3D11Buffer* m_pViewCB;
+    ID3D11Buffer* m_pProjCB;
+    ID3D11Buffer* m_pWorldCB;
+    ID3D11Buffer* m_pCameraPosCB;
+    XMMATRIX m_viewMatrix;
+    XMMATRIX m_projMatrix;
+    CObjParser m_ObjParser;
+    XMFLOAT3 m_SphericalCamera;
+    XMFLOAT3 m_SphericalCameraPos;
+
+    //Color de Luz Difusa
+    ID3D11Buffer* m_pDiffuseColorCB;
+    XMFLOAT3 m_DiffuseColor;
+
+    XMFLOAT3 colores;
+
+    // ---------- Luz Especular
+    ID3D11Buffer* m_pSpecularIntensity;
+    float specularIntensity;
+    ID3D11Buffer* m_pSpecularIntensity2;
+    float specularIntensity2;
+
+    //Posicion de la luz especular
+    XMFLOAT3 m_Light;
+    XMFLOAT3 m_LightPos;
+
+    bool colorCambiox = false;
+    bool colorCambioy = false;
+    bool colorCambioz = false;
+};
+
+
+#endif // _DEMOMODEL_H_
